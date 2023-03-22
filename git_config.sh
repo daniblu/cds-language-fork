a821bca1c4329206d8c802bf25dc7f65efe8644b
daniblu <mr.daniblu@gmail.com>
2023-03-22 11:56:46 +0100

    session8 db nb added

--- git_config.sh
+++
@@ -1,2 +0,0 @@
-git config --global user.email "mr.daniblu@gmail.com"
-git config --global user.name "daniblu"
---
+++ git_env_config.sh
@@ -0,0 +1,6 @@
+git config --global user.email "mr.daniblu@gmail.com"
+git config --global user.name "daniblu"
+
+# install venv
+sudo apt-get update
+sudo apt-get install python3-venv
